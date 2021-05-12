ad2a38585e2ec8c19288a221f26b87bddb1e7cf2
MartinFjellstrom <martin.fjallstrom@gmail.com>
2021-05-12 13:14:26 +0000

    Added a loader to show when the first question is loading

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,9 +3,11 @@ const choices = Array.from(document.getElementsByClassName("choice-text"));
 const progressText = document.getElementById("progress-text");
 const scoreText = document.getElementById("score");
 const progressBarFull = document.getElementById("progress-bar-full");
+const loader = document.getElementById("loader");
+const game = document.getElementById("game");
 
 const correctBonus = 10;
-const maxQuestions = 3;
+const maxQuestions = 5;
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -36,9 +38,9 @@ fetch("https://opentdb.com/api.php?amount=20&category=9&difficulty=easy&type=mul
             answerChoices.forEach(function (choice, index) {
                 formattedQuestion['choice' + (index + 1)] = choice;
             });
-
             return formattedQuestion;
         });
+
         startGame();
     })
     .catch(function (err) {
@@ -50,6 +52,8 @@ function startGame() {
     score = 0;
     availableQuestions = [...questions];
     getNewQuestion();
+    game.classList.remove("hidden");
+    loader.classList.add("hidden");
 };
 
 function getNewQuestion() {
