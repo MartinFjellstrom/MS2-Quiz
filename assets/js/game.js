70c8fb58d027cc5caed05e7ba3d790760ba873db
MartinFjellstrom <martin.fjallstrom@gmail.com>
2021-05-12 10:06:12 +0000

    Added a start game function

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,3 +12,35 @@ let questions = [];
 // CONSTANTS
 const correctBonus = 10;
 const maxQuestions = 15;
+
+function startGame() {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
+};
+
+
+function getNewQuestion() {
+
+    if (availableQuestions.length === 0 || questionCounter >= maxQuestions) {
+        localStorage.setItem("mostRecentScore", score);
+        //Go to the end page
+        return window.location.assign("end.html");
+    };
+
+    questionCounter++;
+
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerHTML = currentQuestion.question;
+
+    choices.forEach(function (choice) {
+        const number = choice.dataset["number"];
+        choice.innerHTML = currentQuestion["choice" + number];
+    });
+
+    availableQuestions.splice(questionIndex, 1);
+
+    acceptingAnswers = true;
+};
