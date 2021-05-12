3c346d2f651df12bf58ab8e32d77748a3b239fd7
MartinFjellstrom <martin.fjallstrom@gmail.com>
2021-05-12 12:27:40 +0000

    added a function that saves the users score to the local storage

--- assets/js/end.js
+++ assets/js/end.js
@@ -3,6 +3,9 @@ const saveScoreBtn = document.getElementById("save-score-btn");
 const finalScore = document.getElementById("final-score");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+const maxHighScores = 5;
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener("keyup", function () {
@@ -11,4 +14,17 @@ username.addEventListener("keyup", function () {
 
 function saveHighScore(e) {
     e.preventDefault();
+
+    const newScore = {
+        score: mostRecentScore,
+        name: username.value
+    };
+    highScores.push(newScore);
+    highScores.sort(function (a, b) {
+        return b.score - a.score;
+    });
+    highScores.splice(5);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("index.html");
 };
