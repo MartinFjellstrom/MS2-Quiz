6bfb064ba41b6252045be926bf5ee6d85b90e748
MartinFjellstrom <martin.fjallstrom@gmail.com>
2021-05-12 09:59:00 +0000

    Created the JS file and some main variables for the game

---
+++ assets/js/game.js
@@ -0,0 +1,14 @@
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [];
+
+// CONSTANTS
+const correctBonus = 10;
+const maxQuestions = 15;
